9200e60a7c2e950e54ec48c8aad9f12bb927edef
Brandon Paredes <brandonparedes21@outlook.com>
2025-03-31 14:05:36 -0600

    Agregar archivos de proyecto.

---
+++ BE.SysProductos.DAL/BESysProductosDbContext.cs
@@ -0,0 +1,34 @@
+﻿using BE.SysProductos.EN;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE.SysProductos.DAL
+{
+    public class BESysProductosDbContext : DbContext
+    {
+        public BESysProductosDbContext(DbContextOptions<BESysProductosDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Producto> Productos { get; set; }
+        public DbSet<Proveedor> Proveedores { get; set; }
+        public DbSet<Compra> Compras { get; set; }
+        public DbSet<DetalleCompra> DetalleCompras { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+
+            modelBuilder.Entity<DetalleCompra>()
+                .HasOne(d => d.Compra)
+                .WithMany(c => c.DetalleCompras)
+                .HasForeignKey(d => d.IdCompra);
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ BE.SysProductos.EN/Producto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE.SysProductos.EN
+{
+    public class Producto
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "El nombre del producto es obligatorio")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "El precio del producto es obligatorio")]
+        public decimal Precio { get; set; }
+        [Required(ErrorMessage = "La cantidad del producto es obligatoria")]
+        public int CantidadDisponible { get; set; }
+        [Required(ErrorMessage = "La Fecha del producto es obligatoria")]
+        public DateTime FechaCreacion { get; set; }
+    }
+}
